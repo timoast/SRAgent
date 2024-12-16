55fea8ee1214702dfd752e1b823117c8682b6479
Nick Youngblut <nyoungb2@gmail.com>
2024-12-16 23:28:27 +0000

    updated settings

--- SRAgent/record_db.py
+++ SRAgent/record_db.py
@@ -2,6 +2,7 @@
 ## batteries
 import os
 import warnings
+from importlib import resources
 from typing import List, Dict, Any, Tuple, Optional
 ## 3rd party
 import psycopg2
@@ -17,8 +18,12 @@ warnings.filterwarnings("ignore", message="pandas only supports SQLAlchemy conne
 # functions
 def db_connect() -> connection:
     """Connect to the sql database"""
+    settings_path = None
+    with resources.path("SRAgent", "settings.yml") as settings_path:
+        settings_path = str(settings_path)
+
     settings = Dynaconf(
-        settings_files=["settings.yml"], 
+        settings_files=["settings_OLD.yml", settings_path], 
         environments=True, 
         env_switcher="DYNACONF"
     )
